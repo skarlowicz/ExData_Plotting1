d8b1b9af22864d8f6634ece78f0da02a470474a1
Sigizmund Karlowicz <github5c72@qkan.org>
2014-10-13 00:40:42 +0400

    initial version

---
+++ plot3.R
@@ -0,0 +1,17 @@
+pw_cons_file <- unz("household_power_cons.zip","household_power_consumption.txt")
+pw <- read.csv(pw_cons_file,header=TRUE,sep=";",stringsAsFactors=FALSE,dec=".",na.strings="?",colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+pw$Date <- as.Date(strptime(pw$Date,"%d/%m/%Y"))
+pw1 <- subset(pw,as.Date(pw$Date) >= as.Date("2007-02-01") & as.Date(pw$Date) <= as.Date("2007-02-02"))
+rm(pw)
+pw1$Time <-  strptime(paste(pw1$Date, pw1$Time), "%Y-%m-%d %H:%M:%S")
+
+png("plot3.png",width=480,height=480,antialias="cleartype")
+par(mfcol=c(1,1),mar=c(5,4,2,1),oma = c(0, 0, 0, 0))
+with(pw1, {
+plot(Time,Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+lines(Time,Sub_metering_1,col="black")
+lines(Time,Sub_metering_2,col="red")
+lines(Time,Sub_metering_3,col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+})
+dev.off()
